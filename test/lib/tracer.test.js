6c9be9bcd601893a8a10d33358c5104fad061410
popomore <sakura9515@gmail.com>
2018-06-20 21:50:14 +0800

    f

--- test/lib/tracer.test.js
+++ test/lib/tracer.test.js
@@ -8,6 +8,7 @@ const SpanContext = require('../../lib/span_context');
 
 describe('test/lib/tracer.test.js', () => {
   let app;
+  let ctx;
   before(async () => {
     app = mm.app('apps/opentracing-app');
     await app.ready();
@@ -15,11 +16,13 @@ describe('test/lib/tracer.test.js', () => {
   after(async () => {
     await app.close();
   });
+  beforeEach(() => {
+    ctx = app.mockContext();
+  });
 
   describe('startSpan', () => {
 
     it('should start and finish', async () => {
-      const ctx = app.mockContext();
       const span = ctx.tracer.startSpan('test');
       await sleep(1);
       span.finish();
@@ -35,7 +38,6 @@ describe('test/lib/tracer.test.js', () => {
     });
 
     it('should startSpan from existing span', async () => {
-      const ctx = app.mockContext();
       const span1 = ctx.tracer.startSpan('test1');
       await sleep(1);
       const span2 = ctx.tracer.startSpan('test2', { childOf: span1 });
@@ -48,7 +50,6 @@ describe('test/lib/tracer.test.js', () => {
     });
 
     it('should startSpan from current span', async () => {
-      const ctx = app.mockContext();
       const span1 = ctx.tracer.startSpan('test1');
       await sleep(1);
       const span2 = ctx.tracer.startSpan('test2');
@@ -63,11 +64,63 @@ describe('test/lib/tracer.test.js', () => {
   });
 
   describe('inject', () => {
+    it('should inject with http carrier', async () => {
+      const span = ctx.tracer.startSpan('test');
+      span.setBaggageItem('a', 1);
+      const carrier = {};
+      ctx.tracer.inject(span, 'HTTP', carrier);
+      assert(carrier['x-b3-traceId'] === span.traceId);
+      assert(carrier['x-b3-spanId'] === span.spanId);
+      assert(carrier['x-b3-baggage'] === '{"a":1}');
+    });
 
+    it('should inject custom carrier return undefined', async () => {
+      app.opentracing.setCarrier('CUSTOM', class {
+        inject() {}
+        extract() {}
+      });
+      const span = ctx.tracer.startSpan('test');
+      const carrier = {};
+      ctx.tracer.inject(span, 'CUSTOM', carrier);
+      assert(Object.keys(carrier).length === 0);
+      assert(typeof carrier === 'object');
+
+      ctx.tracer.inject(span, 'CUSTOM', null);
+    });
+
+    it('should inject carrier with argument spanContext', async () => {
+      let spanContext;
+      app.opentracing.setCarrier('CUSTOM', class {
+        inject(s) {
+          spanContext = s;
+        }
+        extract() {}
+      });
+      const span = ctx.tracer.startSpan('test');
+      ctx.tracer.inject(span, 'CUSTOM', {});
+      assert(spanContext instanceof SpanContext);
+    });
+
+    it('should throw when no carrier is matched', async () => {
+      try {
+        ctx.tracer.inject({}, 'RPC');
+        throw new Error('should not run');
+      } catch (err) {
+        assert(err.message === 'RPC is unknown carrier');
+      }
+    });
   });
 
   describe('extract', () => {
 
+    it('should throw when no carrier is matched', async () => {
+      try {
+        ctx.tracer.extract('RPC');
+        throw new Error('should not run');
+      } catch (err) {
+        assert(err.message === 'RPC is unknown carrier');
+      }
+    });
   });
 
 });
